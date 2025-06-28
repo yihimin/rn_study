31f038653fa924c68dff6654fd94827b5c74a9c6
yihimin <yihimin01@gmail.com>
2025-06-28 16:09:10 +0900

    날씨 API를 통해 날씨 정보를 가져오고, 로딩 상태를 표시하는 기능 추가 및 UI 업데이트

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import { useState, useEffect } from "react";
 import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+const API_KEY = "2ce5f9b07ad8d4623d3501cd8b827e5f";
+
+interface WeatherData {
+  list: Array<{
+    main: {
+      temp: number;
+    };
+    weather: Array<{
+      main: string;
+    }>;
+    dt_txt: string;
+  }>;
+}
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
+  const [days, setDays] = useState<WeatherData | null>(null);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -24,9 +45,14 @@ export default function App() {
       longitude,
     });
     setCity(location[0].city || "Unknown");
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    setDays(json);
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
   return (
     <View style={styles.container}>
@@ -39,18 +65,24 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.weatherName}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.weatherName}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.weatherName}>Sunny</Text>
-        </View>
+        {days === null ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.list.slice(0, 5).map((day, index) => (
+            <View style={styles.day} key={index}>
+              <Text style={styles.temp}>
+                {Math.round(day.main.temp - 273.15)}°
+              </Text>
+              <Text style={styles.weatherName}>{day.weather[0].main}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
